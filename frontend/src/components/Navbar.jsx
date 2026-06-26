b56598652f8a05bfe299ca64d43d170c42270043
sujanbhosle <sujanbhosle26@gmail.com>
2026-06-26 21:17:41 +0530

    Add navigation bar with react router links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div>
-    <nav>
+    <nav
+        style={{
+            display:'flex',
+            justifyContent:"space-between",
+            padding:"20px",
+            backgroundColor:"#242424",
+            color:"white",
+        }}    
+    >
+
         <h2>Ai mock interview</h2>
+        <div>
+            <Link
+                to="/"
+                style={{color:"white",marginRight:"20px",textDecoration:"none"}}>Home
+                </Link>
+            <Link
+                to="/login"
+                style={{color:"white",marginRight:"20px",textDecoration:"none"}}>Login</Link>
+            <Link
+                to="/register"
+                style={{color:"white",textDecoration:"none"}}>Register</Link>
+        </div>
       </nav>
     </div>
   )
