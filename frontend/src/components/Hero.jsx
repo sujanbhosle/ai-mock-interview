fa51b8315d0ed20347d753a86500f9a9ea20cf03
sujanbhosle <sujanbhosle26@gmail.com>
2026-06-27 09:31:27 +0530

    setup tailwind css

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,18 +3,14 @@ import React from 'react'
 export default function Hero() {
   return (
     <div>
-      <section
-        style={{
-            textAlign:'center',
-            padding:"80px 20px",
-        
-        }}
-      >
-        <h1>AI Mock Interview Platform</h1>
-        <p>
+      <section className='text-center py-20 bg-gray-100'>
+        <h1 className='text-5x1 font-bold text-blue-600'>
+            AI Mock Interview Platform</h1>
+        <p className='mt-6 text-lg text-gray-700'>
             practice technical interviews with AI and improve your interview skills
         </p>
-        <button>start interview</button>
+        <button className='mt-8 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700'>
+            start interview</button>
 
       </section>
     </div>
