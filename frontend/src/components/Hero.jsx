da65301e49522a5eb41909cd681cfc422d7bce5b
sujanbhosle <sujanbhosle26@gmail.com>
2026-06-27 09:11:48 +0530

    Add hero section to home page

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+      <section
+        style={{
+            textAlign:'center',
+            padding:"80px 20px",
+        
+        }}
+      >
+        <h1>AI Mock Interview Platform</h1>
+        <p>
+            practice technical interviews with AI and improve your interview skills
+        </p>
+        <button>start interview</button>
+
+      </section>
+    </div>
+  )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <div>
         <Navbar/>
+        <Hero/>
       <h1>Home Page</h1>
     </div>
   )
